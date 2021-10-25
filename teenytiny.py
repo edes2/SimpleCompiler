a2ce21fdf0c54b8925af7d6145f2842506b96278
edes2 <etiennedesclaux@hotmail.com>
2021-10-25 00:16:45 -0400

    first

---
+++ teenytiny.py
@@ -0,0 +1,25 @@
+from lex import *
+from emit import *
+from parse import *
+import sys
+
+#http://web.eecs.utk.edu/~azh/blog/teenytinycompiler1.html
+
+def main():
+    print("Teeny Tiny Compiler")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        input = inputFile.read()
+
+    # Initialize the lexer, emitter, and parser.
+    lexer = Lexer(input)
+    emitter = Emitter("out.c")
+    parser = Parser(lexer, emitter)
+
+    parser.program() # Start the parser.
+    emitter.writeFile() # Write the output to file.
+    print("Compiling completed.")
+
+main()
